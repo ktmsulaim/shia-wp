6b38fe556d5afd76e98ad3b39b399212fefa34a3
K.T MOHAMMED SULAIM <ktmsulaim@gmail.com>
2021-03-25 17:15:41 +0530

    Added malayalam font support for single page

--- wp-content/themes/dhic/page.php
+++ wp-content/themes/dhic/page.php
@@ -16,7 +16,7 @@ pageBanner([
 <section id="page">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 <?php renderMalayalamClass(); ?>">
                <?php the_content(); ?>
             </div>
         </div>
